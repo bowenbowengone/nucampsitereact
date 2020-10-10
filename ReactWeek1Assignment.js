80760019d6aa5c8bd08d819fb47d9986bb96b115
Jennifer Bowen <jrog814@gmail.com>
2020-10-10 12:42:12 -0400

    workshop1

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -1,26 +1,23 @@
-class Student {
-    constructor(name, email, community) {
-        this.name = name;
-        this.email = email;
-        this.community = community;
-    }
-}
-
 class Bootcamp {
     constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
         this.students = students;
+    }
+    registerStudent(studentToRegister) {
+        if(this.students.filter(x => x.email === studentToRegister.email).length > 0) {
+            console.log(`The student ${studentToRegister.email} is already registered.`);
+        } else {
+            this.students.push(studentToRegister);
+            console.log(`Registering ${studentToRegister.email} to the bootcamp ${this.name}.`);
         }
     }
+}
 
-    registerStudent(studentToRegister) {
-        if (this.students.filter(x = x => x.email === student.email).length > 0) {
-            console.log(`The student ${student.email} is already registered.`);
-        } else {
-            this.students.push(student);
-            console.log(`Registering ${student.email} to the bootcamp ${this.name}.`)
-        }
-        return this.students;
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
     }
 }
