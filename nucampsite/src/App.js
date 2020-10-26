16c269b3c2587d7abb75f2fdc61976d63f036036
Jennifer Bowen <jrog814@gmail.com>
2020-10-26 19:19:39 -0400

    Intro to Redux

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -9,15 +9,15 @@ const store = ConfigureStore();
 
 class App extends Component {
   render() {
-      return (
-        <Provider store={store}>
-          <BrowserRouter>
-              <div className="App">
-                  <Main />
-              </div>
-          </BrowserRouter>
-        </Provider>
-      );
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+            <div className="App">
+                <Main />
+            </div>
+        </BrowserRouter>
+      </Provider>
+    );
   }
 }
 
