8991c038a632a78727e358f5badec5203c0de7a7
Jennifer Bowen <jrog814@gmail.com>
2020-11-05 22:31:06 -0500

    Fetch Post Comment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -23,7 +23,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (<div className="col-md-5 m-1">
             <h4>Comments</h4>
@@ -36,7 +36,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     </div>
                 );
             })}
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );       
     }
@@ -73,7 +73,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -174,7 +174,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
